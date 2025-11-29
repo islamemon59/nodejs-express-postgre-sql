99709f914d547c82bb551d3db0fe5731f5dabc29
islamemon59 <islamemon59@gmail.com>
2025-11-29 21:50:36 +0600

    pg db pool added

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,44 @@
-import express, { Request, Response } from "express"
-const app = express()
-const port = 5000
+import express, { Request, Response } from "express";
+import { Pool } from "pg";
+const app = express();
+const port = 5000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello Next Level Developer!')
-})
+//json parser
+app.use(express.json());
+//if need from data
+// app.use(express.urlencoded());
+
+const pool = new Pool({
+  connectionString: `postgresql://neondb_owner:npg_KzMu42yWEwYt@ep-cool-dream-ad2jopvo-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+});
+
+//DB
+const initDB = async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXIST users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    age INT,
+    phone VARCHAR(14),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    )
+        `);
+};
+
+initDB();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Next Level Developer !");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(200).json({ success: true, message: "Api is working" });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
