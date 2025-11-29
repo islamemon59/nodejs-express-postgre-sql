feeebae8c92909fcb0fc126a3f42c656e4c665a5
islamemon59 <islamemon59@gmail.com>
2025-11-29 22:32:37 +0600

    todos table added

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
 import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 const app = express();
 const port = 5000;
 
@@ -9,13 +12,13 @@ app.use(express.json());
 // app.use(express.urlencoded());
 
 const pool = new Pool({
-  connectionString: `postgresql://neondb_owner:npg_KzMu42yWEwYt@ep-cool-dream-ad2jopvo-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+  connectionString: `${process.env.CONNECTION_STR}`,
 });
 
 //DB
 const initDB = async () => {
   await pool.query(`
-    CREATE TABLE IF NOT EXIST users(
+    CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
@@ -23,9 +26,22 @@ const initDB = async () => {
     phone VARCHAR(14),
     address TEXT,
     created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
     )
         `);
+
+  await pool.query(`
+            CREATE TABLE IF NOT EXISTS todos(
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+            title VARCHAR(200) NOT NULL,
+            description TEXT,
+            completed BOOLEAN DEFAULT false,
+            due_date DATE,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `);
 };
 
 initDB();
