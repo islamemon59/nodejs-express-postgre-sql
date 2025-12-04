0bafbfcc6db42642efc3e62c30cda9dd62d38f4f
islamemon59 <islamemon59@gmail.com>
2025-12-04 23:17:31 +0600

    bug fixed

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -3,7 +3,7 @@ import { authControllers } from "./auth.controller";
 
 const router = Router();
 
-//http://localhost:5000/auth/login
+//http://localhost:5000/auth/login  
 router.post("/login", authControllers.loginUser);
 
 export const authRoutes = router;
